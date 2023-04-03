147e028e34a7a16571bf97f71d5e68757e280c01
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-04-03 21:21:08 +0200

    Add original code and one possible solution to WhyUseSupervisor.kt

--- livedemo/src/main/kotlin/com/kotlinconf/workshop/WhyUseSupervisor.kt
+++ livedemo/src/main/kotlin/com/kotlinconf/workshop/WhyUseSupervisor.kt
@@ -1,10 +1,7 @@
 package com.kotlinconf.workshop
 
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
 
 val counterFlow = flow {
@@ -16,19 +13,20 @@ val counterFlow = flow {
 }
 
 suspend fun main() {
-    coroutineScope {
-        // A breaks after 10 iterations
-        launch(CoroutineName("A")) {
-            counterFlow.collect {
-                println("A: $it")
-                if (it >= 10) error("I've had enough!")
-            }
-        }
-        // B is supposed to stay running!
-        launch(CoroutineName("B")) {
-            counterFlow.collect {
-                println("B: $it")
-            }
+    // original code: coroutineScope {
+    val cs = CoroutineScope(SupervisorJob())
+    // A breaks after 10 iterations
+    cs.launch(CoroutineName("A")) {
+        counterFlow.collect {
+            println("A: $it")
+            if (it >= 10) error("I've had enough!")
         }
     }
+    // B is supposed to stay running!
+    cs.launch(CoroutineName("B")) {
+        counterFlow.collect {
+            println("B: $it")
+        }
+    }
+    delay(10000)
 }
