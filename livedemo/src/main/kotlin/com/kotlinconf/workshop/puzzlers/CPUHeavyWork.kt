93e08df1b9dcc0a2ea5bfbd3b4d35f51b8b2cf45
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-04-08 13:47:39 +0200

    Add CPUHeavyWork.kt

--- livedemo/src/main/kotlin/com/kotlinconf/workshop/puzzlers/CPUHeavyWork.kt
+++ livedemo/src/main/kotlin/com/kotlinconf/workshop/puzzlers/CPUHeavyWork.kt
@@ -1,19 +1,20 @@
 package com.kotlinconf.workshop.puzzlers
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
 
-fun doCpuHeavyWork() {
+suspend fun doCpuHeavyWork() {
     Thread.sleep(1000)
 }
 
-
-suspend fun runHeavyTasks() {
-    repeat(10) {
-        doCpuHeavyWork()
-        yield() // let other coroutines run
-    }
-}
-
 suspend fun main() {
-    runHeavyTasks()
+    coroutineScope {
+        launch {
+            repeat(10) {
+                doCpuHeavyWork()
+                yield()
+            }
+        }
+    }
 }
