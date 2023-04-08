5e54da2d6066c0a00c555f9cbb58226453bcc8d6
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-04-08 13:05:04 +0200

    Extract handleSocket function

--- server/src/main/kotlin/com/kotlinconf/workshop/plugins/ChatRouting.kt
+++ server/src/main/kotlin/com/kotlinconf/workshop/plugins/ChatRouting.kt
@@ -6,24 +6,33 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 
 fun Application.configureChatRouting(chat: Chat = Chat()) {
     routing {
-        webSocket("/chat") { // websocketSession
-            val sess = this
-            coroutineScope {
-                launch {
-                    while (true) {
-                        val message = sess.receiveDeserialized<ChatMessage>()
-                        chat.sendMessage(message)
-                    }
-                }
-                launch {
-                    chat.messageFlow.collect {
-                        sess.sendSerialized(it)
-                    }
-                }
+        webSocket("/chat") {
+            handleSocket(this, chat)
+        }
+    }
+}
+
+private suspend fun handleSocket(
+    session: WebSocketServerSession,
+    chat: Chat
+) {
+    coroutineScope {
+        launch {
+            // use socket.receiveDeserialized<ChatMessage>() to receive a message from the WebSocket
+            while (true) {
+                val message = session.receiveDeserialized<ChatMessage>()
+                chat.broadcastMessage(message)
+            }
+        }
+        launch {
+            // use session.sendSerialized(message) to send a message to the WebSocket
+            chat.messageFlow.collect {
+                session.sendSerialized(it)
             }
         }
     }
@@ -31,8 +40,8 @@ fun Application.configureChatRouting(chat: Chat = Chat()) {
 
 class Chat {
     private val _messageFlow: MutableSharedFlow<ChatMessage> = MutableSharedFlow()
-    val messageFlow: MutableSharedFlow<ChatMessage> get() = _messageFlow
-    suspend fun sendMessage(message: ChatMessage) {
+    val messageFlow: SharedFlow<ChatMessage> get() = _messageFlow
+    suspend fun broadcastMessage(message: ChatMessage) {
         _messageFlow.emit(message)
     }
 }
