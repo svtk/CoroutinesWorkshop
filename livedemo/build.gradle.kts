0b926d5a6e49ba767896eca3f272c0b847640a1b
svetlana.isakova <svetlana.isakova@jetbrains.com>
2024-05-10 14:05:22 +0200

    Update turbine and junit dependencies in build.gradle.kts

--- livedemo/build.gradle.kts
+++ livedemo/build.gradle.kts
@@ -23,10 +23,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation(project(mapOf("path" to ":shared")))
-    testImplementation("app.cash.turbine:turbine:0.12.3")
+    testImplementation("app.cash.turbine:turbine:1.1.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.getByName<Test>("test") {
