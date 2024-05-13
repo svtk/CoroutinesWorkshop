455db734e0f2a894a64abd871275a39acadad705
svetlana.isakova <svetlana.isakova@jetbrains.com>
2024-05-13 16:07:34 +0200

    Removed solutions for tasks

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task6Exceptions.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task6Exceptions.kt
@@ -5,8 +5,5 @@ import com.kotlinconf.workshop.articles.network.BlogService
 import kotlinx.coroutines.flow.*
 
 fun observeArticlesUnstable(service: BlogService): Flow<Article> = flow {
-    val list = service.getArticleInfoList()
-    for (articleInfo in list) {
-        emit(Article(articleInfo, service.getCommentsUnstable(articleInfo)))
-    }
+    TODO()
 }
