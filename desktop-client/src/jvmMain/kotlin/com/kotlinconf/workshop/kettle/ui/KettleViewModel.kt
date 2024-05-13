e30a0b3efdfca409e337999deb572b6949e1f1da
svetlana.isakova <svetlana.isakova@jetbrains.com>
2024-05-13 22:06:30 +0200

    Small changes to initial code after review

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/kettle/ui/KettleViewModel.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/kettle/ui/KettleViewModel.kt
@@ -53,9 +53,9 @@ class KettleViewModel(
 
     val smoothCelsiusTemperature: Flow<CelsiusTemperature?> =
         flowOf(null)
-//        celsiusTemperature.map {
-//            it.value
-//        }.averageOfLast(5).map {
-//            CelsiusTemperature(it)
-//        }
+//        celsiusTemperature
+//            .filterNotNull()
+//            .map { it.value }
+//            .averageOfLast(5)
+//            .map { it.celsius }
 }
