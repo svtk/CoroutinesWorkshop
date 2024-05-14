3e863359c4d1db43f0e95400ad2257b3123445b1
Yahor Berdnikau <egorr.berd@gmail.com>
2024-05-14 15:51:24 +0200

    Add 'foojay-resolver' plugin to auto-provision missing JDK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         google()
