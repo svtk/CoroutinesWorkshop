9e5787ac2d41fc21604b51cc02bcba6c68ce253c
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-05-21 19:31:59 +0200

    Move Flow demo solution to FlowIntroDemoVersion

--- livedemo/src/main/kotlin/com/kotlinconf/workshop/flows/FlowIntroDemo.kt
+++ livedemo/src/main/kotlin/com/kotlinconf/workshop/flows/FlowIntroDemo.kt
@@ -6,29 +6,6 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
-// Finite flow
-// Collect once
-// Collect twice
-// Infinite flow
-
 suspend fun main(): Unit = coroutineScope {
-    val numbers = flow<Int> {
-        var x = 0
-        while (true) {
-            println("Generated $x")
-            emit(x++)
-            delay(500)
-        }
-    }
-    launch {
-        numbers.collect {
-            println("A Received $it")
-            delay(2000)
-        }
-    }
-    launch {
-        numbers.collect {
-            println("B Received $it")
-        }
-    }
+
 }
