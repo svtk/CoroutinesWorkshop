2fd99cad7ebf85dee2cd501aa7ee26471cf034a9
Alejandro Serrano Mena <alejandro.serrano.mena@jetbrains.com>
2025-05-07 09:55:27 +0200

    Use a scrollbar

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/ui/views/ArticlesView.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/ui/views/ArticlesView.kt
@@ -1,9 +1,11 @@
 package com.kotlinconf.workshop.articles.ui.views
 
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -59,14 +61,21 @@ fun ActiveUsersView(activeUsers: Set<User>) {
 fun ArticlesView(
     articles: List<Article>
 ) {
-    LazyColumn {
-        item {
-            ThreeColumn("Author", "Title", "Number of Comments", FontWeight.ExtraBold)
+    Box(modifier = Modifier.fillMaxHeight()) {
+        val scrollState = rememberScrollState()
+        Column(
+            modifier = Modifier.fillMaxSize().verticalScroll(scrollState)
+        ) {
+            ThreeColumn("Author", "Title", "# comments", FontWeight.ExtraBold)
+            for (article in articles) {
+                Spacer(modifier = Modifier.height(10.dp))
+                ArticleView(article)
+            }
         }
-        items(articles) { article ->
-            Spacer(modifier = Modifier.height(10.dp))
-            ArticleView(article)
-        }
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(scrollState)
+        )
     }
 }
 
