ea896fd05a11f05e1f3f83dfbe2606846ef81c20
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-05-17 15:26:19 +0200

    tighten scope for task 7

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task7Retry.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task7Retry.kt
@@ -8,11 +8,16 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.toList
 
+// observeArticlesUnstableWithRetry is already implemented for you.
 fun observeArticlesUnstableWithRetry(service: BlogService): Flow<Article> = flow {
-    TODO()
+    val list = service.getArticleInfoList()
+    for (articleInfo in list) {
+        emit(Article(articleInfo, getCommentsWithRetry(service, articleInfo)))
+    }
 }
 
 suspend fun getCommentsWithRetry(service: BlogService, articleInfo: ArticleInfo): List<Comment> {
-    return service.getCommentsUnstable(articleInfo)
+    return service.getCommentsUnstable(articleInfo) // TODO: Implement me correctly!
 }
