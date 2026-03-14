7d2e7220ebedd0a9a8322f5c316e5103f9e3d6f2
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2026-03-14 18:49:41 +0100

    Enable `-Xexplicit-backing-fields` compiler option (StateFlow & SharedFlow encapsulation)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,28 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+}
+
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        extensions.configure<KotlinJvmProjectExtension> {
+            @OptIn(ExperimentalKotlinGradlePluginApi::class)
+            compilerOptions {
+                freeCompilerArgs.add("-Xexplicit-backing-fields")
+            }
+        }
+    }
+
+    plugins.withId("org.jetbrains.kotlin.multiplatform") {
+        extensions.configure<KotlinMultiplatformExtension> {
+            @OptIn(ExperimentalKotlinGradlePluginApi::class)
+            compilerOptions {
+                freeCompilerArgs.add("-Xexplicit-backing-fields")
+            }
+        }
+    }
 }
