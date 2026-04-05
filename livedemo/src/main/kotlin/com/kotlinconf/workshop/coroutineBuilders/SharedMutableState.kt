416be11c9ee4aab145f1b77179b3e99b7ac0a3c0
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2026-04-05 20:43:25 +0200

    Replace `AtomicInteger` with `AtomicInt` and adopt `incrementAndFetch` from kotlin.concurrent.atomics

--- livedemo/src/main/kotlin/com/kotlinconf/workshop/coroutineBuilders/SharedMutableState.kt
+++ livedemo/src/main/kotlin/com/kotlinconf/workshop/coroutineBuilders/SharedMutableState.kt
@@ -1,15 +1,20 @@
+@file:OptIn(ExperimentalAtomicApi::class)
+
 package com.kotlinconf.workshop.coroutineBuilders
 
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.atomics.AtomicInt
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.atomics.incrementAndFetch
 
 suspend fun main() {
-    var x = AtomicInteger(0)
+    val x = AtomicInt(0)
     coroutineScope {
         repeat(10_000) {
             launch {
-                x.incrementAndGet()
+                x.incrementAndFetch()
             }
         }
     }
