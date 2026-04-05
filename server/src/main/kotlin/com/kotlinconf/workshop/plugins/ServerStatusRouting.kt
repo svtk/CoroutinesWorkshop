666d09972ce3783260d71135312a55e9903cc7bc
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2026-04-05 22:24:16 +0200

    Enhance root endpoint to display server capabilities and supported routes.

--- server/src/main/kotlin/com/kotlinconf/workshop/plugins/ServerStatusRouting.kt
+++ server/src/main/kotlin/com/kotlinconf/workshop/plugins/ServerStatusRouting.kt
@@ -7,7 +7,22 @@ import io.ktor.server.routing.*
 fun Application.configureStatusRouting() {
     routing {
         get("/") {
-            call.respondText("The server is running")
+            call.respondText(
+                """
+                The server is running. Here's its capabilities:
+                - GET /articles
+                - GET /articles/{id}/comments
+                - GET /articles/{id}/comments?failure=0.3
+                - GET /wallet/earn
+                - GET /wallet/money
+                - POST /kettle/on
+                - POST /kettle/off
+                - GET /kettle/temperature
+                - GET /kettle/temperature?failure=0.3
+                - WS /kettle-ws
+                - WS /chat
+                """.trimIndent()
+            )
         }
     }
 }
